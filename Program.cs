1d204721901adcf59a208275849b6557a8b3ee00
Alex Mickelson <alex.mickelson@snow.edu>
2023-08-22 14:48:44 -0600

    last weeks lab implemented

--- Program.cs
+++ Program.cs
@@ -1,16 +1,34 @@
-﻿Console.WriteLine("Hello, World!");
-var helloWorld = "Hello, World!";
-// 'h' + 'e'
+﻿
+var secret = "abcd";
+var isDone = false;
 
-// for(int i = 0; i < helloWorld.Length; i++)
-// {
-//   Console.WriteLine(helloWorld[i]);
-// }
+Console.WriteLine("Welcome to my guessing game");
 
-
-int i = 0;
-while(i < helloWorld.Length)
+var guessCount = 0;
+while (!isDone)
 {
-  Console.WriteLine(helloWorld[i]);
-  i++;
+  guessCount += 1;
+  Console.WriteLine($"Guess #{guessCount}, input a 4 letter, lowercase value:");
+  var userInput = Console.ReadLine();
+
+  var numberOfPositionsCorrect = 0;
+  var numberOfLettersCorrect = 0;
+
+  for (var i = 0; i < secret.Length; i++)
+  {
+    var userInputLetter = userInput[i];
+    var secretLetter = secret[i];
+
+    if (userInputLetter == secretLetter)
+      numberOfPositionsCorrect += 1;
+
+    if (secret.Contains(userInputLetter))
+      numberOfLettersCorrect += 1;
+  }
+
+  Console.WriteLine($"You got {numberOfLettersCorrect} letters correct and {numberOfPositionsCorrect} positions correct");
+
+  isDone = numberOfPositionsCorrect == secret.Length;
 }
+
+Console.WriteLine($"You Guessed It! it took you {guessCount} tries");
