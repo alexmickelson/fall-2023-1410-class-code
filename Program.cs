8400d007c0c47535d7ebc1bbedb7434d5583ab66
Alex Mickelson <alex.mickelson@snow.edu>
2023-08-23 09:25:18 -0600

    wednesday class discussion

--- Program.cs
+++ Program.cs
@@ -1,12 +1,29 @@
 ﻿
 var secret = "abcd";
-var isDone = false;
 
-Console.WriteLine("Welcome to my guessing game");
-
-var guessCount = 0;
-while (!isDone)
+static int NumberOfPositionsCorrect(int numberOfPositionsCorrect, char userInputLetter, char secretLetter)
 {
+  if (userInputLetter == secretLetter)
+    numberOfPositionsCorrect += 1;
+  return numberOfPositionsCorrect;
+}
+
+static int GetNumberOfLetterCorrect(string secret, string userInput, int numberOfLettersCorrect, int index) // parameters
+{
+  for (int j = 0; j < userInput.Length; j++)
+  {
+    if (userInput[index] == secret[j])
+    {
+      numberOfLettersCorrect += 1;
+    }
+  }
+
+  return numberOfLettersCorrect;
+}
+
+static bool HandleUserGuess(string secret, ref int guessCount)
+{
+  bool isDone;
   guessCount += 1;
   Console.WriteLine($"Guess #{guessCount}, input a 4 letter, lowercase value:");
   var userInput = Console.ReadLine();
@@ -19,16 +36,24 @@ while (!isDone)
     var userInputLetter = userInput[i];
     var secretLetter = secret[i];
 
-    if (userInputLetter == secretLetter)
-      numberOfPositionsCorrect += 1;
-
-    if (secret.Contains(userInputLetter))
-      numberOfLettersCorrect += 1;
+    numberOfPositionsCorrect = NumberOfPositionsCorrect(numberOfPositionsCorrect, userInputLetter, secretLetter);
+    numberOfLettersCorrect = GetNumberOfLetterCorrect(secret, userInput, numberOfLettersCorrect, i); //arguments
   }
 
   Console.WriteLine($"You got {numberOfLettersCorrect} letters correct and {numberOfPositionsCorrect} positions correct");
 
   isDone = numberOfPositionsCorrect == secret.Length;
+  return isDone;
+}
+
+var isDone = false;
+
+Console.WriteLine("Welcome to my guessing game");
+
+var guessCount = 0;
+while (!isDone)
+{
+  isDone = HandleUserGuess(secret, ref guessCount);
 }
 
 Console.WriteLine($"You Guessed It! it took you {guessCount} tries");
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # fall-2023-1410-class-code
 
+## How to save something in git
+
+1. git add --all // git add .
+2. git commit -m "made a change"
+3. git push
+
+## How to get things out of git
+
+1. clone
+2. pull
+
+configure your user for git
+
+```bash
+git config --global user.name "Alex Mickelson"
+git config --global user.email "alex.mickelson@snow.edu"
+```
+
+
+## Lab Game Plan
+
+1. while we arent done handle the users guess
+   1. get user input
+   2. check position and character count
+   3. ouput to user
+   4. see if we need to go again
+2. tell user end report
+
+## whats the difference
+
+Computer Science - discover new things
+Software Engineering - optimization and application
