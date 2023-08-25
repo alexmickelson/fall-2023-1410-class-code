e9627f0a43e1ec4020046d56f5f5668fc19bcf10
Alex Mickelson <alex.mickelson@snow.edu>
2023-08-25 08:58:19 -0600

    working in case i break it later

--- sorting/Program.cs
+++ sorting/Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+// Console.ReadLine();
+
+// File.ReadAllText("./nubmers.txt");
+// File.WriteAllText("./numbers-out.txt", "");
+
+var rawNumbers = File.ReadAllText("./numbers.txt");
+
+
+var numbers = new List<int>();
+
+var rawNumbersArray = rawNumbers.Split(' ');
+
+foreach(var rawNumber in rawNumbersArray)
+{
+  numbers.Add(int.Parse(rawNumber));
+  // double.Parse
+  // char.Parse();
+
+}
+
+foreach(var number in numbers)
+{
+  Console.WriteLine(number);
+}
