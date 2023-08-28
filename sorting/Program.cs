d5c3b0482d67a4dd09fb049b4694b7aa9c5ffbc2
Alex Mickelson <alex.mickelson@snow.edu>
2023-08-28 08:08:49 -0600

    class friday

--- sorting/Program.cs
+++ sorting/Program.cs
@@ -5,6 +5,26 @@
 // File.ReadAllText("./nubmers.txt");
 // File.WriteAllText("./numbers-out.txt", "");
 
+static List<int> SelectionSort(List<int> input)
+{
+  for (int i = 0; i < input.Count(); i++)
+  {
+    var largestIndex = 0;
+    if(input[largestIndex] < input[i])
+    {
+      largestIndex = i;
+    }
+
+    var lastIndex = input.Count() - 1 - i;
+
+    var lastValue = input[lastIndex];
+    input[lastIndex] = input[largestIndex];
+    input[largestIndex] = lastValue;
+
+  }
+  return input;
+}
+
 var rawNumbers = File.ReadAllText("./numbers.txt");
 
 
@@ -14,13 +34,26 @@ var rawNumbersArray = rawNumbers.Split(' ');
 
 foreach(var rawNumber in rawNumbersArray)
 {
-  numbers.Add(int.Parse(rawNumber));
-  // double.Parse
-  // char.Parse();
-
+  try 
+  {
+    var parsedNumber = int.Parse(rawNumber);
+    // if(parsedNumber < 1000)
+    // {
+    //   throw new Exception("number was less than 1000");
+    // }
+    numbers.Add(parsedNumber);
+  }
+  catch(Exception)
+  {
+    Console.WriteLine("something happened");
+  }
 }
 
-foreach(var number in numbers)
+numbers = new List<int>() { 4, 3, 2, 1 };
+
+var sortedList = SelectionSort(numbers);
+
+foreach(var number in sortedList)
 {
   Console.WriteLine(number);
 }
