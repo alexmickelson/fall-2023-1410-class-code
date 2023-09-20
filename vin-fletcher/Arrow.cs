d53af4a6126cd88d471f3cd1768e2fccd1b28eca
Alex Mickelson <alex.mickelson@snow.edu>
2023-09-20 07:54:46 -0600

    prepping for class

--- vin-fletcher/Arrow.cs
+++ vin-fletcher/Arrow.cs
@@ -13,7 +13,25 @@ public class Arrow
 
   public double GetCost()
   {
-    return 2.3;
+    double cost = 0;
+
+    if (Point == ArrowHead.Steel)
+      cost += 10;
+    if (Point == ArrowHead.Wood)
+      cost += 3;
+    if (Point == ArrowHead.Obsidian)
+      cost += 5;
+
+    if(FletchingMaterial == Fletching.Plastic)
+      cost += 10;
+    if(FletchingMaterial == Fletching.TurkeyFeathers)
+      cost += 5;
+    if(FletchingMaterial == Fletching.GooseFeathers)
+      cost += 3;
+
+    cost += .05 * ShaftLength;
+
+    return cost;
   }
 
   public double GetShaftLenght()
